d49893c014a90f47a93436e8b4ca27c364545751
Syafiq <syafiq.rezpector@gmail.com>
2019-10-22 23:02:25 +0700

    remove unnecessary function

--- draw/src/main/java/com/divyanshu/draw/widget/DrawView.kt
+++ draw/src/main/java/com/divyanshu/draw/widget/DrawView.kt
@@ -2,200 +2,23 @@ package com.divyanshu.draw.widget
 
 import android.content.Context
 import android.graphics.*
-import androidx.annotation.ColorInt
-import androidx.core.graphics.ColorUtils
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import com.divyanshu.draw.widget.contract.*
-import com.divyanshu.draw.widget.mode.PathMode
-import java.util.*
-import kotlin.collections.ArrayList
 
-class DrawView(context: Context, attrs: AttributeSet) : View(context, attrs), CanvasContract, DrawingModeContract, PaintContract {
+class DrawView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private var _drawingMode: DrawingMode = DrawingMode.LINE
-    private var _color = 0
-    private var _strokeWidth = 0F
-    private var _alpha = 0
-
-    override var drawingMode: DrawingMode
-        get() = _drawingMode
-        set(value) {
-            _drawingMode = value
-            destroyDrawingObject()
-        }
-
-    override var color: Int
-        get() = _color
-        set(value) {
-            @ColorInt
-            val alphaColor = ColorUtils.setAlphaComponent(value, alpha)
-            _color = alphaColor
-            mPath?.color = alphaColor
-        }
-    override var strokeWidth: Float
-        get() = _strokeWidth
-        set(value) {
-            _strokeWidth = value
-            mPath?.strokeWidth = value
-        }
-    override var alpha: Int
-        get() = _alpha
-        set(value) {
-            _alpha = (value*255)/100
-            color = color
-        }
-
-    private var mPaths = LinkedList<DrawingHolderContract>()
-    private var mPathsR = LinkedList<DrawingHolderContract>()
-
-    private var mLastPaths = LinkedList<DrawingHolderContract>()
-    private var mUndonePaths = LinkedList<DrawingHolderContract>()
-
-    private var stepState = LinkedList<DrawingStepState>()
-    private var lastState = LinkedList<DrawingStepState>()
-
-    private var mPaint = Paint()
-    private var mPath :DrawingHolderContract? = null
-
-    private var mIsSaving = false
-
-    var isEraserOn = false
-        private set
-
-    init {
-        mPaint.apply {
-            style = Paint.Style.STROKE
-            strokeJoin = Paint.Join.ROUND
-            strokeCap = Paint.Cap.ROUND
-            isAntiAlias = true
-        }
-    }
-
-    fun undo() {
-        if (mPaths.isEmpty() && mLastPaths.isNotEmpty()) {
-            mPaths.addAll(mLastPaths)
-            mPathsR.addAllFirst(mLastPaths)
-            mLastPaths.clear()
-            invalidate()
-            return
-        }
-        if (mPaths.isEmpty()) {
-            return
-        }
-
-        val last = mPaths.removeLast()
-        mPathsR.removeFirst()
-
-        mUndonePaths.addLast(last)
-        invalidate()
-    }
-
-    fun redo() {
-        if (mUndonePaths.isEmpty()) {
-            return
-        }
-
-        val last = mUndonePaths.removeLast()
-        mPaths.addLast(last)
-        mPathsR.addFirst(last)
-        invalidate()
-    }
 
     fun getBitmap(): Bitmap {
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(bitmap)
         canvas.drawColor(Color.TRANSPARENT)
-        mIsSaving = true
         draw(canvas)
-        mIsSaving = false
         return bitmap
     }
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        mPaths.forEach {
-            it.onDraw(canvas, mPaint)
-        }
-
-        mPath?.onDraw(canvas, mPaint)
-    }
-
-    fun clearCanvas() {
-        stepState.addLast(DrawingStepState.CLEAR)
-
-        mLastPaths.clear()
-        mLastPaths.addAll(mPaths)
-        mPaths.clear()
-        mPathsR.clear()
-        destroyDrawingObject()
-        invalidate()
-    }
-
-    private fun actionDown(x: Float, y: Float) {
-        mPath?.onFingerDown(x, y)
-    }
-
-    private fun actionMove(x: Float, y: Float) {
-        mPath?.onFingerMove(x, y)
-    }
-
-    private fun actionUp(x: Float, y: Float) {
-        mPath?.onFingerUp(x, y)
-    }
-
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        if(mPath == null)
-            createDrawingObject()
-
-        if(mPath?.onTouchEvent(event) == true) {
-            val x = event.x
-            val y = event.y
-
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> actionDown(x, y)
-                MotionEvent.ACTION_MOVE -> actionMove(x, y)
-                MotionEvent.ACTION_UP -> actionUp(x, y)
-            }
-        }
-
         return true
     }
-
-    override fun attachToCanvas() {
-        val path = mPath
-        if(path != null)
-        {
-            stepState.addLast(DrawingStepState.DRAW)
-
-            mPaths.addLast(path)
-            mPathsR.addFirst(path)
-            destroyDrawingObject()
-        }
-    }
-
-    override fun requestInvalidate() {
-        invalidate()
-    }
-
-    override fun createDrawingObject() {
-        mPath = when(drawingMode) {
-            DrawingMode.LINE -> PathMode(this).apply {
-                alpha = this@DrawView.alpha
-                color = this@DrawView.color
-                strokeWidth = this@DrawView.strokeWidth
-            }
-            else -> null
-        }
-    }
-
-    override fun destroyDrawingObject() {
-        mPath = null
-    }
-}
-
-fun <T> LinkedList<T>.addAllFirst(c: Collection<T>): Boolean{
-    c.forEach(this::addFirst)
-    return true
 }
