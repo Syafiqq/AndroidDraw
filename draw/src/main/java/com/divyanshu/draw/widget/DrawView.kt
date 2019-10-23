296b3cb14215c858c25c7f45c4b873d881a576a6
Syafiq <syafiq.rezpector@gmail.com>
2019-10-23 21:27:48 +0700

    add working undo and redo

--- draw/src/main/java/com/divyanshu/draw/widget/DrawView.kt
+++ draw/src/main/java/com/divyanshu/draw/widget/DrawView.kt
@@ -10,8 +10,17 @@ import android.view.View
 import com.divyanshu.draw.widget.component.PathContainer
 import com.divyanshu.draw.widget.contract.CanvasContract
 import com.divyanshu.draw.widget.contract.DrawingMode
+import com.divyanshu.draw.widget.contract.design.command.ICommand
+import com.divyanshu.draw.widget.contract.design.command.ICommandManager
+import com.divyanshu.draw.widget.mode.PathMode
+import java.util.*
+import kotlin.collections.ArrayList
 
-class DrawView(context: Context, attrs: AttributeSet) : View(context, attrs), CanvasContract {
+class DrawView(context: Context, attrs: AttributeSet) : View(context, attrs), CanvasContract, ICommandManager {
+    override val recordF = Stack<ICommand>()
+    override val recordB = Stack<ICommand>()
+    private val holder = ArrayList<PathMode>()
+
     private var _drawingMode: DrawingMode = DrawingMode.LINE
     val drawingTools: Map<DrawingMode, PathContainer> = mapOf(
             DrawingMode.LINE to PathContainer(context, this)
@@ -29,9 +38,23 @@ class DrawView(context: Context, attrs: AttributeSet) : View(context, attrs), Ca
     }
 
     fun undo() {
+        if (recordF.isEmpty()) return
+
+        val command = recordF.pop()
+        command.down()
+        recordB.push(command)
+
+        requestInvalidate()
     }
 
     fun redo() {
+        if (recordB.isEmpty()) return
+
+        val command = recordB.pop()
+        command.up()
+        recordF.push(command)
+
+        requestInvalidate()
     }
 
     override fun onDraw(canvas: Canvas) {
