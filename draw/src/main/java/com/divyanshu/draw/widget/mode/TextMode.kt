5e89db952b5b5296fff3ade33acd546fb05e203e
Syafiq <syafiq.rezpector@gmail.com>
2019-10-26 14:28:21 +0700

    add text mode

---
+++ draw/src/main/java/com/divyanshu/draw/widget/mode/TextMode.kt
@@ -0,0 +1,43 @@
+package com.divyanshu.draw.widget.mode
+
+import android.graphics.Paint
+import com.divyanshu.draw.widget.contract.DrawingMode
+import com.divyanshu.draw.widget.contract.IMode
+
+class TextMode(override val mode: DrawingMode) : IMode {
+    var color = 0
+    var textSize = 0F
+
+    private var curX = 0F
+    private var curY = 0F
+    private var initX = 0F
+    private var initY = 0F
+
+    fun onFingerDown(x: Float, y: Float) {
+        initialPos(x, y)
+        currentPos(x, y)
+    }
+
+    fun onFingerMove(x: Float, y: Float) {
+        currentPos(x, y)
+    }
+
+    fun onFingerUp(x: Float, y: Float) {
+        currentPos(x, y)
+    }
+
+    private fun currentPos(x: Float, y: Float) {
+        curX = x
+        curY = y
+    }
+
+    private fun initialPos(x: Float, y: Float) {
+        initX = x
+        initY = y
+    }
+
+    fun decorate(paint: Paint) {
+        paint.textSize = textSize
+        paint.color = color
+    }
+}
