cf7dad6c6f7c8d46962ecfd23c3692af9669e89a
Syafiq <syafiq.rezpector@gmail.com>
2019-10-27 06:15:08 +0700

    add image container

---
+++ draw/src/main/java/com/divyanshu/draw/widget/container/ImageContainer.kt
@@ -0,0 +1,101 @@
+package com.divyanshu.draw.widget.container
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.MotionEvent
+import com.divyanshu.draw.widget.contract.*
+import com.divyanshu.draw.widget.mode.ImageMode
+import java.io.InputStream
+
+class ImageContainer(override val context: Context, override val drawing: ICanvas) : IDrawingContainer, IImageDrawCallback {
+    private var draw: ImageMode? = null
+
+    private val listener: InteractionListener
+
+    private val paint = Paint()
+
+    init {
+        val ctx = this.context
+        if (ctx !is InteractionListener) {
+            throw ClassCastException("context must implement InteractionListener")
+        }
+
+        listener = ctx
+    }
+
+    override fun onDraw(canvas: Canvas, draw: IMode) {
+        if (draw !is ImageMode) return
+        draw.onDraw(canvas, paint)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        draw?.let { onDraw(canvas, it) }
+    }
+
+    override fun createDrawingObject(x: Float, y: Float) {
+        if (draw != null) return
+
+        listener.attachComponent(this)
+        draw = ImageMode(DrawingMode.IMAGE).apply {
+            currentPos(x, y)
+        }
+        listener.requestImage()
+    }
+
+    override fun destroyDrawingObject() {
+        draw = null
+        listener.detachComponent()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (draw?.bitmap == null) return true
+
+        val x = event.x
+        val y = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> draw?.onFingerDown(x, y)
+            MotionEvent.ACTION_MOVE -> draw?.onFingerMove(x, y)
+            MotionEvent.ACTION_UP ->  draw?.onFingerUp(x, y)
+        }
+
+        drawing.requestInvalidate()
+        return true
+    }
+
+    override fun onImageRetreived(image: InputStream) {
+        draw?.run {
+            updateBitmapFromStream(image)
+            drawing.requestInvalidate()
+        }
+    }
+
+    override fun onApply() {
+        draw?.run {
+            drawing.attachToCanvas(this)
+            drawing.requestInvalidate()
+        }
+    }
+
+    override fun onCancel() {
+        destroyDrawingObject()
+        drawing.requestInvalidate()
+    }
+
+    override fun onScaleUp() {
+        draw?.scaledUp(paint)
+        drawing.requestInvalidate()
+    }
+
+    override fun onScaleDown() {
+        draw?.scaledDown(paint)
+        drawing.requestInvalidate()
+    }
+
+    interface InteractionListener {
+        fun attachComponent(callback: IImageDrawCallback)
+        fun requestImage()
+        fun detachComponent()
+    }
+}
