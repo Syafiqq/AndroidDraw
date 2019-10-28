12fc5ef56da12f6a3e5c356ff2423747fc016f31
Syafiqq <muhammad.syafiq@beautyfulminds.com.sg>
2019-10-28 13:35:56 +0700

    pas motion event

--- draw/src/main/java/com/divyanshu/draw/widget/contract/IDrawingContainer.kt
+++ draw/src/main/java/com/divyanshu/draw/widget/contract/IDrawingContainer.kt
@@ -6,7 +6,7 @@ import android.view.MotionEvent
 
 
 interface IDrawingContainer {
-    fun createDrawingObject(x: Float, y: Float)
+    fun createDrawingObject(x: Float, y: Float, event:MotionEvent)
     fun destroyDrawingObject()
     fun onDraw(canvas: Canvas, draw: IMode)
     fun onDraw(canvas: Canvas)
